922a425f00bc5c300b9d120093fe73c3921a5253
RM24004 <167376455+RM24004@users.noreply.github.com>
2026-09-24 22:57:42 -0600

    Base Backend
    Base del sistema Sigtau

---
+++ backend/src/main/java/com/sigtau/sigtau/SigtauApplication.java
@@ -0,0 +1,13 @@
+package com.sigtau.sigtau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigtauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigtauApplication.class, args);
+	}
+
+}
